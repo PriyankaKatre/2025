5000bca8ff04c0c84e9db4d1e8524825ab6e49be
Priyanka Katre <priyankack1707@gmail.com>
2025-01-06 19:16:40 +0000

    integrate doctors availability end point

--- server/controllers/doctorController.js
+++ server/controllers/doctorController.js
@@ -1,13 +1,14 @@
-import doctorModel from "./../models/doctorModel";
+import doctorModel from "../models/doctorModel.js";
 
 const chnageDoctorsAvailability = async (req, res) => {
   try {
-    const docId = req.body;
+    const { docId } = req.body;
+    console.log("docId", req.body);
     const doctorData = await doctorModel.findById(docId);
     await doctorModel.findByIdAndUpdate(docId, {
       available: !doctorData.available,
     });
-    return res.status(400).json({
+    return res.status(200).json({
       success: true,
       message: "Doctor's availability changed successfully",
     });
