656f4b9010b3a041cc7d43e929ad4c59dfd09960
Priyanka Katre <priyankack1707@gmail.com>
2025-02-25 15:52:36 +0000

    Rotate & Slide Effect

---
+++ js50Days/Day03/index.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const open = document.querySelector('.open-btn');
+    const close = document.querySelector('.close-btn');
+    const container = document.querySelector('.container');
+
+    open.addEventListener('click', () => {
+        container.classList.add('show-nav');
+    })
+    close.addEventListener('click', () => {
+      container.classList.remove('show-nav');
+    });
+})
